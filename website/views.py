f8c8e7f164842a9afc68f222d871f6f7c4b6bef1
rhhtahina <rhhtahina@gmail.com>
2025-08-05 21:00:10 +0300

    Register

--- website/views.py
+++ website/views.py
@@ -41,3 +41,5 @@ def register_user(request):
     else:
         form = SignUpForm()
         return render(request, 'register.html', {'form': form})
+    
+    return render(request, 'register.html', {'form': form})
